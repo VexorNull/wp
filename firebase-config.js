891cb26f6a8f78c343ffdfd68fd26bbfcf304b4e
Tanveer Hussain <67091803+VexorNull@users.noreply.github.com>
2026-09-11 00:02:13 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Keep this file separate to avoid exposing your API keys when pushing to GitHub.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAP2fvQksImno3ONsZJShgGIHsKc1EU8KM",
+  authDomain: "wordpress-c971f.firebaseapp.com",
+  databaseURL: "https://wordpress-c971f-default-rtdb.firebaseio.com",
+  projectId: "wordpress-c971f",
+  storageBucket: "wordpress-c971f.firebasestorage.app",
+  messagingSenderId: "828580995189",
+  appId: "1:828580995189:web:b79bcdc6fd60f441ce983c",
+  measurementId: "G-CQQEJLR5TY"
+};
